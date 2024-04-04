c6620fd8bf0edbd9fbef9a189db98226caf3f3cb
Nandini <nandini2000.rohatgi@gmail.com>
2024-04-04 17:56:50 +0530

    fixed hamburger bug

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,40 +1,45 @@
-import React from 'react'
-import './Header.scss'
-import logo from '../../utils/newLogo.jpg'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import './Header.scss';
+import logo from '../../utils/newLogo.jpg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     const [showMenu, setShowMenu] = useState(false);
-  
+
     const toggleMenu = () => {
-      setShowMenu(!showMenu);
+        setShowMenu(!showMenu);
     };
- 
-  return (
-    <div className={`main-header ${showMenu ? "show-menu" : ""}`}>
-    <div className="logo">
-     <img src={logo} alt="logo"></img>
-    </div>
 
-    <div className='navbar'>
+    // Function to close the menu
+    const closeMenu = () => {
+        setShowMenu(false);
+    };
 
-    <nav className={`nav-links ${showMenu ? "show-menu" : ""}`}>
-    <Link to={"/"}> Home</Link>
-    <Link to={"/services"}>Services</Link>
-    <Link to={"/about"}> About Us</Link>
-    <Link to={"/contact"}>Contact Us</Link>
+    return (
+        <div className={`main-header ${showMenu ? "show-menu" : ""}`}>
+            <div className="logo">
+                <img src={logo} alt="logo"></img>
+            </div>
 
-    </nav>
-    </div>
-    <div className="hamburger-icon" onClick={toggleMenu}>
-    &#9776; 
-  </div>
-  <div className='profile'>
+            <div className='navbar'>
+                <nav className={`nav-links ${showMenu ? "show-menu" : ""}`}>
+                    <Link to={"/"} onClick={closeMenu}> Home</Link>
+                    <Link to={"/services"} onClick={closeMenu}>Services</Link>
+                    <Link to={"/about"} onClick={closeMenu}> About Us</Link>
+                    <Link to={"/contact"} onClick={closeMenu}>Contact Us</Link>
+                </nav>
+            </div>
 
-  </div>
-  </div>
-  )
+            <div className="hamburger-icon" onClick={toggleMenu}>
+                &#9776;
+            </div>
+
+            <div className='profile'>
+                {/* Add profile content here */}
+            </div>
+        </div>
+    );
 }
 
-export default Header
+export default Header;
+
